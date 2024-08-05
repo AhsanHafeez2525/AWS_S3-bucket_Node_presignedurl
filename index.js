f4a117f3c202699235a26181a0dac09b37f4236a
Ahsan Hafeez Satti <ahsansatti402@gmail.com>
2024-08-06 02:44:32 +0500

    presigned-putobject-complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { GetObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const { GetObjectCommand, S3Client, PutObjectCommand, ListObjectsV2Command, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 console.log("AWS_REGION:", process.env.AWS_REGION);
@@ -24,11 +24,51 @@ async function getObjectURL(key) {
   return url;
 }
 
+async function putObject (filename, contentType){
+const command = new PutObjectCommand({
+    Bucket: "newahsanbucket",
+    Key: `uploads/user-uploads/${filename}`,
+    ContentType: contentType,
+  
+})
+const url = await getSignedUrl(s3Client, command)
+return url
+}
+async function listObjects(){
+    const command = new ListObjectsV2Command({
+        Bucket: "newahsanbucket",
+        key: '/'
+    })
+    const result = await s3Client.send(command);
+  console.log(result)
+}
+async function initList() {
+//   await listObjects()
+
+//   delete a object 
+const cmd = new DeleteObjectCommand({
+  key: "video-1722893371187.mp4",
+  Bucket: "newahsanbucket",
+});
+await s3Client.send(cmd);
+}
+
+initList();
 async function init() {
-  console.log(
-    "URL for Screenshot 2024-07-09 160432.png",
-    await getObjectURL("Screenshot 2024-07-09 160432.png")
-  );
+  //   console.log(
+  //     "URL for Screenshot 2024-07-09 160432.png",
+  //     await getObjectURL("Screenshot 2024-07-09 160432.png")
+  //   );
+
+  // to retreive the upload image
+//   console.log(
+//     "URL for Screenshot 2024-07-09 160432.png",
+//     await getObjectURL(process.env.AWS_UPLOAD_URL)
+//   );
+  // console.log('URL for uploading', await putObject(`image-${Date.now()}.jpeg`, 'image/jpeg'))
+
+//   to upload video
+//   console.log('URL for uploading', await putObject(`video-${Date.now()}.mp4`, 'video/mp4'))
 }
 
 init();
